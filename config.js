97d2263dfdc143a46e162ef3b235900dca841f5e
GKedarnath <72261336+GKedarnath@users.noreply.github.com>
2022-01-07 17:14:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyAvLOKj3LxsCxBHQrVBQ6YxX8kAnJn7RFw",
+  authDomain: "story-telling-app-b703c.firebaseapp.com",
+  databaseURL: "https://story-telling-app-b703c-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-b703c",
+  storageBucket: "story-telling-app-b703c.appspot.com",
+  messagingSenderId: "453291245333",
+  appId: "1:453291245333:web:ca914129abb85e3a34731c"
+};
